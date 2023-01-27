c2f6a8db00b6b3be29cffc12422ae8f09fb50231
Joey Tsui <joeytsui131@gmail.com>
2023-01-27 16:37:22 -0500

    pushed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Route, Switch } from "react-router-dom";
 
 import PokemonBrowser from "./components/PokemonBrowser";
-
+import { getPokemon } from "./store/pokemon";
 const App = () => {
   return (
     <Switch>
